08a64e2a23193f197f906402f87b73a610512be4
gdiaz <geraldo.diaz@outlook.com>
2018-09-27 23:40:13 -0400

    Support HTTP Authorization Token

--- API_Consumer/Procedures/APICaller_GET.cs
+++ API_Consumer/Procedures/APICaller_GET.cs
@@ -24,7 +24,29 @@ public partial class StoredProcedures
         }
         catch (Exception ex)
         {
-            Helper.SendResultValue("Result", ex.Message.ToString());
+            Helper.SendResultValue("Error", ex.Message.ToString());
+        }
+    }
+
+    /// <summary>
+    /// It's a generic procedure used to consume Api throught GET method.
+    /// Returns the result as a varchar(max). Could be used to return Json.
+    /// </summary>
+    /// <param name="URL">Api GET Method</param>
+    /// <param name="Token">Authorization Token</param>
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static void APICaller_GET(SqlString URL, SqlString Token)
+    {
+        try
+        {
+            string Result = APIConsumer.GETMethod(URL.ToString(), Token.ToString());
+
+            Helper.SendResultValue("Result", Result);
+
+        }
+        catch (Exception ex)
+        {
+            Helper.SendResultValue("Error", ex.Message.ToString());
         }
     }
 }
