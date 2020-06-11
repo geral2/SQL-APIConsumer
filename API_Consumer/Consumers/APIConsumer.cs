caa9bf2dca0db46d4c452a3cfebb262e9c2c845e
gdiaz <geraldo.diaz@outlook.com>
2020-06-10 23:29:25 -0400

    Added APICaller_GET_Extended Added APICaller_POST_Extended Added APICaller_POST_Encoded
    Now, every CLR SQL Procedure will return an exectution value -1, if occurs an exception.

--- API_Consumer/Consumers/APIConsumer.cs
+++ API_Consumer/Consumers/APIConsumer.cs
@@ -20,6 +20,11 @@ namespace SQLAPI_Consumer
         private const string CONTENTTYPE = "application/json; charset=utf-8";
 
         /// <summary>
+        /// Fixed Context type supported.
+        /// </summary>
+        private const string CONTENTTYPE_URLENCODED = "application/x-www-form-urlencoded";
+
+        /// <summary>
         /// Fixed string for POST method
         /// </summary>
         private const string POST_WebMethod = "POST";
@@ -28,6 +33,26 @@ namespace SQLAPI_Consumer
         /// Fixed string for GET method
         /// </summary>
         private const string GET_WebMethod = "GET";
+
+        /// <summary>
+        /// DEFAULT_EXECUTION_RESULT
+        /// </summary>
+        public const int DEFAULT_EXECUTION_RESULT = 0;
+
+        /// <summary>
+        /// FAILED_EXECUTION_RESULT
+        /// </summary>
+        public const int FAILED_EXECUTION_RESULT = -1;
+
+        /// <summary>
+        /// DEFAULT_COLUMN_RESULT
+        /// </summary>
+        public const string DEFAULT_COLUMN_RESULT = "Result";
+
+        /// <summary>
+        /// DEFAULT_COLUMN_RESULT
+        /// </summary>
+        public const string DEFAULT_COLUMN_ERROR = "Error";
 
         /// <summary>
         /// POST to Resful API sending Json body.
@@ -135,9 +160,7 @@ namespace SQLAPI_Consumer
                     {
                         ContentResult = ex.Message.ToString();
                     }
-                    
                 }
-                
             }
             catch (Exception ex)
             {
@@ -180,6 +203,199 @@ namespace SQLAPI_Consumer
                 {
                     if(!String.IsNullOrEmpty(JsonBody))
                         streamWriter.Write(JsonBody);
+
+                    streamWriter.Flush();
+                }
+
+                var httpResponse = (HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    ContentResult = result;
+                }
+            }
+            catch (WebException ex)
+            {
+                using (var stream = ex.Response?.GetResponseStream())
+                {
+                    if (stream != null)
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            var result = reader.ReadToEnd();
+                            ContentResult = result;
+                        }
+                    }
+                    else
+                    {
+                        ContentResult = ex.Message.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ContentResult = ex.Message.ToString();
+                throw ex;
+            }
+
+            return ContentResult;
+        }
+
+        /// <summary>
+        /// POST to Resful API sending Json body.
+        /// </summary>
+        /// <param name="url">API URL</param>
+        /// <param name="JsonBody">Content Application By Default Json</param>
+        /// <param name="JsonHeaders">Headers added in Json format: Authorization token, user-passwrod, JWT, etc.</param>
+        /// <returns>String Api result</returns>
+        public static string POSTMethod_Extended(ref ExtendedResult extResult, string url, string JsonBody = "", string JsonHeaders = "")
+        {
+            string ContentResult = string.Empty;
+            try
+            {
+                SetSSL();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                //request.ContentType = ContentType;
+                request.Method = POST_WebMethod;
+
+                if (!string.IsNullOrEmpty(JsonHeaders))
+                {
+                    List<Headers> _headers = JsonConvert.DeserializeObject<List<Headers>>(JsonHeaders);
+
+                    foreach (var Header in _headers)
+                    {
+                        if (!string.IsNullOrEmpty(Header.Name) && !string.IsNullOrEmpty(Header.Value))
+                        {
+                            if (Header.Name.Contains("Content-Type"))
+                            {
+                                request.ContentType = Header.Value;
+                            }
+                            else
+                            {
+                                request.Headers.Add(Header.Name, Header.Value);
+                            }
+                        }
+                    }
+                }
+
+                if (request.ContentType.ToLower() == CONTENTTYPE_URLENCODED.ToLower())
+                {
+                    byte[] byteArray = System.Text.Encoding.UTF8.GetBytes((!String.IsNullOrEmpty(JsonBody)) ? JsonBody : "");
+                    // Set the ContentLength property of the WebRequest.  
+                    request.ContentLength = byteArray.Length;
+
+                    using (var streamWriter = request.GetRequestStream())
+                    {
+                        streamWriter.Write(byteArray, 0, byteArray.Length);
+                        // Close the Stream object.  
+                        streamWriter.Close();
+                        // Get the response.  
+
+                        streamWriter.Flush();
+                    }
+                }
+                else
+                {
+                    using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                    {
+                        if (!String.IsNullOrEmpty(JsonBody))
+                            streamWriter.Write(JsonBody);
+
+                        streamWriter.Flush();
+                    }
+                }
+
+                var httpResponse = (HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    ContentResult = result;
+
+                    extResult.ContentType = httpResponse.ContentType;
+                    extResult.Server = httpResponse.Server;
+                    extResult.Result = ContentResult;
+                    extResult.StatusCode = httpResponse.StatusCode.ToString();
+                    extResult.StatusDescription = httpResponse.StatusDescription;
+
+                    for (int i = 0; i < httpResponse.Headers.Count; ++i)
+                    {
+                        extResult.headers.Add(
+                                                   new Headers()
+                                                   {
+                                                       Name = httpResponse.Headers.Keys[i],
+                                                       Value = httpResponse.Headers[i]
+                                                   }
+                                            );
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                using (var stream = ex.Response?.GetResponseStream())
+                {
+                    if (stream != null)
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            var result = reader.ReadToEnd();
+                            ContentResult = result;
+                        }
+                    }
+                    else
+                    {
+                        ContentResult = ex.Message.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ContentResult = ex.Message.ToString();
+                throw ex;
+            }
+
+            return ContentResult;
+        }
+
+
+        /// <summary>
+        /// POST to Resful API sending Json body.
+        /// </summary>
+        /// <param name="url">API URL</param>
+        /// <param name="JsonBody">Content Application By Default Json</param>
+        /// <param name="JsonHeaders">Headers added in Json format: Authorization token, user-passwrod, JWT, etc.</param>
+        /// <returns>String Api result</returns>
+        public static string POSTMethod_urlencoded(string url, string JsonBody, string JsonHeaders = "")
+        {
+            string ContentResult = string.Empty;
+            try
+            {
+                SetSSL();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.ContentType = CONTENTTYPE_URLENCODED;
+                request.Method = POST_WebMethod;
+
+                if (!string.IsNullOrEmpty(JsonHeaders))
+                {
+                    List<Headers> _headers = JsonConvert.DeserializeObject<List<Headers>>(JsonHeaders);
+
+                    foreach (var Header in _headers)
+                    {
+                        if (!string.IsNullOrEmpty(Header.Name) && !string.IsNullOrEmpty(Header.Value))
+                            request.Headers.Add(Header.Name, Header.Value);
+                    }
+                }
+
+                byte[] byteArray = System.Text.Encoding.UTF8.GetBytes((!String.IsNullOrEmpty(JsonBody)) ? JsonBody : "");
+                // Set the ContentLength property of the WebRequest.  
+                request.ContentLength = byteArray.Length;
+
+                using (var streamWriter = request.GetRequestStream())
+                {
+                    streamWriter.Write(byteArray, 0, byteArray.Length);
+                    // Close the Stream object.  
+                    streamWriter.Close();
+                    // Get the response.  
+
 
                     streamWriter.Flush();
                 }
@@ -534,6 +750,121 @@ namespace SQLAPI_Consumer
                     var result = streamReader.ReadToEnd();
                     ObjectResult = JsonConvert.DeserializeObject<T>(result);
                     ContentResult = result;
+                }
+            }
+            catch (WebException ex)
+            {
+                using (var stream = ex.Response?.GetResponseStream())
+                {
+                    if (stream != null)
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            var result = reader.ReadToEnd();
+                            ContentResult = result;
+                        }
+                    }
+                    else
+                    {
+                        ContentResult = ex.Message.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ContentResult = ex.Message.ToString();
+                throw ex;
+            }
+
+            return ContentResult;
+        }
+         
+        /// <summary>
+        /// Request GET Method to the URL API provided.
+        /// </summary>
+        /// <param name="url">API URL</param>
+        /// <param name="Authorization">Header Authorization</param>
+        /// <returns>String Api result</returns>
+        public static string GETMethod_Extended(ref ExtendedResult extResult, string url, string JsonBody = "", string Headers = "" )
+        {
+            string ContentResult = string.Empty;
+            try
+            {
+                SetSSL();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                //request.ContentType = ContentType;
+                request.Method = GET_WebMethod;
+
+                if (!string.IsNullOrEmpty(Headers))
+                {
+                    List<Headers> _headers = JsonConvert.DeserializeObject<List<Headers>>(Headers);
+
+                    foreach (var Header in _headers)
+                    {
+                        if (!string.IsNullOrEmpty(Header.Name) && !string.IsNullOrEmpty(Header.Value))
+                        {
+                            if (Header.Name.Contains("Content-Type"))
+                            {
+                                request.ContentType = Header.Value;
+                            }
+                            else
+                            {
+                                request.Headers.Add(Header.Name, Header.Value);
+                            }
+                        }
+
+                    }
+                }
+
+                if (request.ContentType.ToLower() == CONTENTTYPE_URLENCODED.ToLower())
+                {
+                    byte[] byteArray = System.Text.Encoding.UTF8.GetBytes((!String.IsNullOrEmpty(JsonBody)) ? JsonBody : "");
+                    // Set the ContentLength property of the WebRequest.  
+                    request.ContentLength = byteArray.Length;
+
+                    using (var streamWriter = request.GetRequestStream())
+                    {
+                        streamWriter.Write(byteArray, 0, byteArray.Length);
+                        // Close the Stream object.  
+                        streamWriter.Close();
+                        // Get the response.  
+
+                        streamWriter.Flush();
+                    }
+                }
+                else
+                {
+                    using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                    {
+                        if (!String.IsNullOrEmpty(JsonBody))
+                            streamWriter.Write(JsonBody);
+
+                        streamWriter.Flush();
+                    }
+                }
+
+                var httpResponse = (HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    ContentResult = result;
+                    extResult.ContentType = httpResponse.ContentType;
+                    extResult.Server = httpResponse.Server;
+                    extResult.Result = ContentResult;
+                    extResult.StatusCode = httpResponse.StatusCode.ToString();
+                    extResult.StatusDescription = httpResponse.StatusDescription;
+
+                    for (int i = 0; i < httpResponse.Headers.Count; ++i)
+                    {
+                        extResult.headers.Add(
+                                                   new Headers()
+                                                   {
+                                                       Name = httpResponse.Headers.Keys[i],
+                                                       Value = httpResponse.Headers[i]
+                                                   }
+                                            );
+                    }
+
                 }
             }
             catch (WebException ex)
--- API_Consumer/Procedures/APICaller_GET.cs
+++ API_Consumer/Procedures/APICaller_GET.cs
@@ -1,5 +1,8 @@
+//using Microsoft.SqlServer.Server;
+//using Newtonsoft.Json;
 using SQLAPI_Consumer;
 using System;
+using System.Data;
 using System.Data.SqlTypes;
 
 /// <summary>
@@ -13,19 +16,24 @@ public partial class StoredProcedures
     /// </summary>
     /// <param name="URL">Api GET Method</param>
     [Microsoft.SqlServer.Server.SqlProcedure]
-    public static void APICaller_GET (SqlString URL)
+    public static SqlInt32 APICaller_GET (SqlString URL)
     {
+        SqlInt32 ExecutionResult = APIConsumer.DEFAULT_EXECUTION_RESULT;
+
         try
         {
             string Result = APIConsumer.GETMethod(URL.ToString());
 
-            Helper.SendResultValue("Result", Result);
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_RESULT, Result);
 
         }
         catch (Exception ex)
         {
-            Helper.SendResultValue("Error", ex.Message.ToString());
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_ERROR, ex.Message.ToString());
+            ExecutionResult = APIConsumer.FAILED_EXECUTION_RESULT;
         }
+
+        return ExecutionResult;
     }
 
     /// <summary>
@@ -35,19 +43,24 @@ public partial class StoredProcedures
     /// <param name="URL">Api GET Method</param>
     /// <param name="Authorization">Authorization Header</param>
     [Microsoft.SqlServer.Server.SqlProcedure]
-    public static void APICaller_GET_Auth(SqlString URL, SqlString Authorization)
+    public static SqlInt32 APICaller_GET_Auth(SqlString URL, SqlString Authorization)
     {
+        SqlInt32 ExecutionResult = APIConsumer.DEFAULT_EXECUTION_RESULT;
+
         try
         {
             string Result = APIConsumer.GETMethod(URL.ToString(), Authorization.ToString());
 
-            Helper.SendResultValue("Result", Result);
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_RESULT, Result);
 
         }
         catch (Exception ex)
         {
-            Helper.SendResultValue("Error", ex.Message.ToString());
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_ERROR, ex.Message.ToString());
+            ExecutionResult = APIConsumer.FAILED_EXECUTION_RESULT;
         }
+
+        return ExecutionResult;
     }
 
     /// <summary>
@@ -57,19 +70,24 @@ public partial class StoredProcedures
     /// <param name="URL">Api GET Method</param>
     /// <param name="Headers">Json Headers</param>
     [Microsoft.SqlServer.Server.SqlProcedure]
-    public static void APICaller_GET_Headers(SqlString URL, SqlString Headers)
+    public static SqlInt32 APICaller_GET_Headers(SqlString URL, SqlString Headers)
     {
+        SqlInt32 ExecutionResult = APIConsumer.DEFAULT_EXECUTION_RESULT;
+
         try
         {
             string Result = APIConsumer.GETMethod_Headers(URL.ToString(), Headers.ToString());
 
-            Helper.SendResultValue("Result", Result);
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_RESULT, Result);
 
         }
         catch (Exception ex)
         {
-            Helper.SendResultValue("Error", ex.Message.ToString());
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_ERROR, ex.Message.ToString());
+            ExecutionResult = APIConsumer.FAILED_EXECUTION_RESULT;
         }
+
+        return ExecutionResult;
     }
 
     /// <summary>
@@ -80,18 +98,53 @@ public partial class StoredProcedures
     /// <param name="Headers">Json Headers</param>
     /// <param name="JsonBody">Json Body</param>
     [Microsoft.SqlServer.Server.SqlProcedure]
-    public static void APICaller_GET_JsonBody_Header(SqlString URL, SqlString JsonBody , SqlString Headers)
+    public static SqlInt32 APICaller_GET_JsonBody_Header(SqlString URL, SqlString Headers, SqlString JsonBody)
     {
+        SqlInt32 ExecutionResult = APIConsumer.DEFAULT_EXECUTION_RESULT;
         try
         {
             string Result = APIConsumer.GETMethod_Headers(URL.ToString(),JsonBody.ToString(), Headers.ToString());
 
-            Helper.SendResultValue("Result", Result);
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_RESULT, Result);
 
         }
         catch (Exception ex)
         {
-            Helper.SendResultValue("Error", ex.Message.ToString());
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_ERROR, ex.Message.ToString());
+            ExecutionResult = APIConsumer.FAILED_EXECUTION_RESULT;
         }
+
+        return ExecutionResult;
+    }
+
+    /// <summary>
+    /// It's a generic procedure used to consume Api throught GET method.
+    /// Returns the result as a varchar(max). Could be used to return Json.
+    /// Content Type must be suplied as headers
+    /// </summary>
+    /// <param name="URL">Api GET Method</param>
+    /// <param name="Headers">Json Headers</param>
+    /// <param name="JsonBody">Json Body</param>
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static SqlInt32 APICaller_GET_Extended(SqlString URL,  SqlString JsonBody, SqlString Headers)
+    {
+        SqlInt32 ExecutionResult = APIConsumer.DEFAULT_EXECUTION_RESULT;
+        try
+        {
+            API_Consumer.ExtendedResult ExtResult = new API_Consumer.ExtendedResult();
+
+            string Result = APIConsumer.GETMethod_Extended(ref ExtResult, URL.ToString(), JsonBody.ToString(), Headers.ToString());
+
+            Helper.SendResultValue(ExtResult);
+            
+            //  Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_RESULT, Result);
+        }
+        catch (Exception ex)
+        {
+            Helper.SendResultValue(APIConsumer.DEFAULT_COLUMN_ERROR, ex.Message.ToString());
+            ExecutionResult = APIConsumer.FAILED_EXECUTION_RESULT;
+        }
+
+        return ExecutionResult;
     }
 }
